8ffe4e115c204b1972012c809d90ab40b6cb3c69
Luiz Antonio Bissoni <labissoni@gmail.com>
2018-08-14 16:52:00 -0300

    Ajuste do projeto

---
+++ SistemaFinanceiro/Models/Categoria.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace SistemaFinanceiro.Models
+{
+    public class Categoria
+    {
+        public int Id { get; set; }
+        public int Id_Categoria { get; set; }
+        public double Salario { get; set; }
+        public double Contas { get; set; }
+        public double Alimentacao { get; set; }
+        public double Moradia { get; set; }
+        public double Saude { get; set; }
+        public double Impostos_taxas { get; set; }
+        public double  Roupas_Acessorios { get; set; }
+    }
+}
---
+++ SistemaFinanceiro/Models/Login.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace SistemaFinanceiro.Models
+{
+    public class Login
+    {
+        public int Id { get; set; }
+        public string Usuario { get; set; }
+        public string Senha { get; set; }
+        public string Email { get; set; }
+    }
+}
