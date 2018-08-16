cf81df82d76411c4368b9f48f0dfa2ff9e2e4766
Leonardo Forbici <38699633+LeonardoForbici@users.noreply.github.com>
2018-08-16 16:00:06 -0300

    Classe repositorio Excluir

--- SistemaFinanceiro/Repositório/RepositorioFinanceiroExcluir.cs
+++ SistemaFinanceiro/Repositório/RepositorioFinanceiroExcluir.cs
@@ -1,5 +1,8 @@
-﻿using System;
+﻿using SistemaFinanceiro.DataBase;
+using SistemaFinanceiro.Models;
+using System;
 using System.Collections.Generic;
+using System.Data.SqlClient;
 using System.Linq;
 using System.Web;
 
@@ -7,5 +10,47 @@ namespace SistemaFinanceiro.Repositório
 {
     public class RepositorioFinanceiroExcluir
     {
+        public bool ExcluirCartoes(int id)
+        {
+            SqlCommand comando = new DBconnection().GetConnction();
+            comando.CommandText = "DELETE FROM cartoes WHERE cartoes id = @ID";
+            comando.Parameters.AddWithValue("@ID", id);
+            return comando.ExecuteNonQuery() == 1;
+        }
+        public bool ExcluirCategoria(int id)
+        {
+            SqlCommand comando = new DBconnection().GetConnction();
+            comando.CommandText = "DELETE FROM cartoes WHERE categoria id = @ID";
+            comando.Parameters.AddWithValue("@ID", id);
+            return comando.ExecuteNonQuery() == 1;
+        }
+        public bool ExcluirGastos(int id)
+        {
+            SqlCommand comando = new DBconnection().GetConnction();
+            comando.CommandText = "DELETE FROM cartoes WHERE gastos id = @ID";
+            comando.Parameters.AddWithValue("@ID", id);
+            return comando.ExecuteNonQuery() == 1;
+        }
+        public bool ExcluirLogin(int id)
+        {
+            SqlCommand comando = new DBconnection().GetConnction();
+            comando.CommandText = "DELETE FROM cartoes WHERE login id = @ID";
+            comando.Parameters.AddWithValue("@ID", id);
+            return comando.ExecuteNonQuery() == 1;
+        }
+        public bool ExcluirPessoas(int id)
+        {
+            SqlCommand comando = new DBconnection().GetConnction();
+            comando.CommandText = "DELETE FROM cartoes WHERE pessoas id = @ID";
+            comando.Parameters.AddWithValue("@ID", id);
+            return comando.ExecuteNonQuery() == 1;
+        }
+        public bool ExcluirRecebimento(int id)
+        {
+            SqlCommand comando = new DBconnection().GetConnction();
+            comando.CommandText = "DELETE FROM cartoes WHERE recebimento id = @ID";
+            comando.Parameters.AddWithValue("@ID", id);
+            return comando.ExecuteNonQuery() == 1;
+        }
     }
 }
