aaa2c7624c3ae72a0f0c93235f27a07e6b1d46fa
BarbaraKosela <38699532+BarbaraKosela@users.noreply.github.com>
2018-08-16 14:51:49 -0300

    .

--- SistemaFinanceiro/Models/Gastos.cs
+++ SistemaFinanceiro/Models/Gastos.cs
@@ -9,18 +9,18 @@ namespace SistemaFinanceiro.Models
     public class Gastos
     {
         public int Id { get; set; }
-        public int IdGastos { get; set; }
+        public int Id_Gastos { get; set; }
 
         [Required(ErrorMessage = "O valor dos gastos não pode ser vazio")]
-        public double ValorDosGastos { get; set; }
+        public double Valor_Dos_Gastos { get; set; }
 
         [DataType(DataType.Date)]
         [DisplayFormat(DataFormatString = "{0:yyyy-MM-dd}", ApplyFormatInEditMode = true)]
-        public DateTime DataDeEntrada { get; set; }
+        public DateTime Data_De_Entrada { get; set; }
 
         [DataType(DataType.Date)]
         [DisplayFormat(DataFormatString = "{0:yyyy-MM-dd}", ApplyFormatInEditMode = true)]
-        public DateTime DataDeVencimento { get; set; }
+        public DateTime Data_De_Vencimento { get; set; }
 
         [Required(ErrorMessage = "A descrição não pode ser vazia")]
         [MaxLength(50, ErrorMessage = "A descrição não pode ultrapassar de cinquenta caracteres")]
