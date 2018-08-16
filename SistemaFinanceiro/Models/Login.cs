1acf2532060b783ddf06a3d07252a6c2215ead30
Luiz Antonio Bissoni <labissoni@gmail.com>
2018-08-16 14:45:40 -0300

    class library criado

--- SistemaFinanceiro/Models/Login.cs
+++ SistemaFinanceiro/Models/Login.cs
@@ -17,22 +17,5 @@ namespace SistemaFinanceiro.Models
         public string Email { get; set; }
     }
 
-    public class UserBussinessLogic
-    {
-        string connectionString = ConfigurationManager.ConnectionStrings["DefaultConnection"].ConnectionString;
-
-        public int CheckUserLogin(Login user)
-        {
-            using (SqlConnection connection = new SqlConnection(connectionString))
-            {
-                SqlCommand command = new SqlCommand("uspLogin", connection);
-                command.CommandType = CommandType.StoredProcedure;
-                command.Parameters.Add(new SqlParameter("@UserLogin", user.Usuario));
-                command.Parameters.Add(new SqlParameter("@Password", user.Senha));
-                connection.Open();
-
-                return Convert.ToInt32(command.ExecuteScalar());
-            }
-        }
-    }
+   
 }
