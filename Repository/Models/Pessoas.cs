8f79a4d77bd8d3752a38f9dc3506c8766bc02b2c
Leonardo Forbici <38699633+LeonardoForbici@users.noreply.github.com>
2018-08-17 14:56:03 -0300

    Mandando models pra cima

---
+++ Repository/Models/Pessoas.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace SistemaFinanceiro.Models
+{
+    public class Pessoas
+    {
+        public int Id { get; set; }
+        public int Id_pessoas { get; set; }
+        public string Nome { get; set; }
+        public int Idade { get; set; }
+        public bool Sexo { get; set; }
+        public int CPF { get; set; }
+        public DateTime Data_nascimento { get; set; }
+        public int Telefone { get; set; }
+        
+    }
+}
