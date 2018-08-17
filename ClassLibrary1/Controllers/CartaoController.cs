1dc7e9e5d2970f7dcc3481537d1e8ffb442120a8
Leonardo Forbici <38699633+LeonardoForbici@users.noreply.github.com>
2018-08-17 15:18:47 -0300

    Add class on Controller

---
+++ ClassLibrary1/Controllers/CartaoController.cs
@@ -0,0 +1,67 @@
+﻿using SistemaFinanceiro.Models;
+using SistemaFinanceiro.Repositório;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace SistemaFinanceiro.Controllers
+{
+    public class CartaoController : Controller
+    {
+     
+       
+        // GET: Financeiro
+        [HttpGet]
+        public ActionResult Index()
+        {
+            List<Cartoes> cartoes = new RepositorioCartoes().ObterTodosCartoes();
+            ViewBag.Cartoes = cartoes;
+            ViewBag.TituloPagina = "Cartões";
+            return View();
+
+        }
+
+        public ActionResult Cadastro()
+        {
+            ViewBag.TituloPagina = "Cartões - Cadastro";
+            ViewBag.Cartao = new Cartoes();
+            return View();
+        }
+
+        public ActionResult Store()
+        {
+            return View();
+        }
+
+        public ActionResult Excluir()
+        {
+            return View();
+        }
+
+        public ActionResult Editar()
+        {
+            return View();
+        }
+
+        public ActionResult Update()
+        {
+            return View();
+        }
+        public ActionResult Login()
+        {
+            return View();
+        }
+
+        public ActionResult ValidarLogin(Login user)
+        {
+          
+            return View();
+        }
+        public ActionResult CreatLogin()
+        {
+            return View();
+        }
+    }
+}
---
+++ ClassLibrary1/Controllers/RecebimentoController.cs
@@ -0,0 +1,50 @@
+﻿using SistemaFinanceiro.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace SistemaFinanceiro.Controllers
+{
+    public class RecebimentoController : Controller
+    {
+        // GET: Recebimento
+        public ActionResult Index()
+        {
+            List<Recebimento> recebimentos = new Recebimento().ObterTodosRecebimentos();
+            ViewBag.Recebimentos = recebimentos;
+            ViewBag.TituloPagina = "Recebimentos";
+            return View();
+        }
+        public ActionResult Cadastro()
+        {
+            ViewBag.TituloPagina = "Recebimento - Cadastro";
+            ViewBag.Recebimento = new Recebimento();
+            return View();
+        }
+          public ActionResult Store()
+        {
+            return View();
+        }
+
+        public ActionResult Excluir()
+        {
+            return View();
+        }
+
+        public ActionResult Editar()
+        {
+            return View();
+        }
+
+        public ActionResult Update()
+        {
+            return View();
+        }
+        public ActionResult Login()
+        {
+            return View();
+        }
+    }
+}
