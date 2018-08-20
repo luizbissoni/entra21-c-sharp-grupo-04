f05a9ace4ab6fbabc3edbb56a0d3d80c7bea00fc
Luiz Antonio Bissoni <labissoni@gmail.com>
2018-08-20 20:18:13 -0300

    add exemplo usando ajax
    exemplo incompleto

---
+++ SistemaFinanceiro/Scripts/ScriptPessoaCadastro/pessoas-cadastro.js
@@ -0,0 +1,55 @@
+﻿$(function () {
+    $("#modal-index-pessoas").on("click", function () {
+        if ($("#modal-cadastro-pessoas").length == 0) {
+            $.ajax({
+                url: "/Pessoas/Cadastro",
+                method: "get",
+                success: function (data) {
+                    $("body").append(data);
+                    $("modal-cadastro-pessoas").modal('show');
+                }
+            });
+        } else {
+            $('modal-cadastro-pessoas').modal('show');
+
+        }
+    });
+
+    $('body').on('click', '#modal-savar-pessoa', function () {
+        $.ajax({
+            url: '/Pessoas/Cadastro',
+            method: 'post',
+            data: {
+                nome: $('#nome').val(),
+                idade: $('#idade').val(),
+                salario: $('#salario').val()
+            },
+            success: function (data) {
+                var resultado = JSON.parse(data);
+                limparCampos();
+                $('#modal-cadastro-pessoas').modal('hide');
+                adicionarLinhaTabela($('nome'.val(), $('#idade').val(), $('#salario').val(), resultado.id));
+                }
+            });
+    });
+
+    function adicionarLinhaTabela(nome, idade, salario, id) {
+        $registro = "<tr>";
+        $registro += "<td>" + nome + "</td>";
+        $registro += "<td>" + idade + "</td>";
+        $registro += "<td>" + salario + "</td>";
+        $registro += "<td><a href=\"/funcionario/Editar?id=" + id + "\" >Editar</a></td>";
+        $registro += "</tr>";
+        $("table").append($registro);
+    }
+
+    function limparCampos() {
+        $("#nome").val("");
+        $("#idade").val("");
+        $("#salario").val("");
+    }
+
+
+
+
+})
