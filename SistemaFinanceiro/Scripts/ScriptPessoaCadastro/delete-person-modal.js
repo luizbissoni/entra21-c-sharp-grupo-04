3e850203ff66449d9a06b3f7274532021b618921
Luiz Antonio Bissoni <labissoni@gmail.com>
2018-08-23 17:25:51 -0300

    editar, excluir com modal

---
+++ SistemaFinanceiro/Scripts/ScriptPessoaCadastro/delete-person-modal.js
@@ -0,0 +1,21 @@
+﻿$(function () {
+    var id = -1;
+
+    $('.excluir-pessoa').on('click', function () {
+        id = $(this).data('id');
+    });
+
+    $('#yes-option').on("click", function () {
+        $.ajax({
+            url: '/Pessoas/Excluir',
+            method: 'GET',
+            data: {
+                id: id
+            },
+            success: function (data) {
+                $('#linha-'+id).remove();
+                $('#exampleModal').modal('hide');
+            }
+        });
+    });
+});
---
+++ SistemaFinanceiro/Scripts/ScriptPessoaCadastro/edit-person-modal.js
@@ -0,0 +1,21 @@
+﻿$(function () {
+    var id = -1;
+    $('.editar-pessoa').on('click', function () {
+        id = $(this).data('id');
+    });
+
+    $('#edit-option').on('click', function () {
+        $.ajax({
+            url: 'Pessoas/Update',
+            method: 'POST',
+            data: {
+                id: id
+            },
+
+            success: function (data) {
+                $('#linha-' + id).add();
+                $('#editar-pessoa-modal').modal('hide');
+            }
+        });
+    });
+});
