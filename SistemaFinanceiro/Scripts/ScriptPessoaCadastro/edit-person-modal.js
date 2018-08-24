072728014dbce00e28bcd99edacd79b5bcff94ee
Luiz Antonio Bissoni <labissoni@gmail.com>
2018-08-23 23:04:00 -0300

    modal editar

--- SistemaFinanceiro/Scripts/ScriptPessoaCadastro/edit-person-modal.js
+++ SistemaFinanceiro/Scripts/ScriptPessoaCadastro/edit-person-modal.js
@@ -1,4 +1,5 @@
-﻿$(function () {
+﻿/// <reference path="C:\Users\T-Gamer\Documents\GitHub\entra21-c-sharp-grupo-04\SistemaFinanceiro\Views/Pessoas/EditarModal.cshtml" />
+$(function () {
     var id = -1;
     $('.editar-pessoa').on('click', function () {
         id = $(this).data('id');
@@ -6,15 +7,15 @@
 
     $('#edit-option').on('click', function () {
         $.ajax({
-            url: 'Pessoas/Update',
-            method: 'POST',
+            url: '/Pessoas/Update',
+            method: 'GET',
             data: {
                 id: id
             },
 
             success: function (data) {
-                $('#linha-' + id).add();
-                $('#editar-pessoa-modal').modal('hide');
+                $('#linha-'+id).remove();
+                $('#editar-pessoa-modal').modal('show');
             }
         });
     });
