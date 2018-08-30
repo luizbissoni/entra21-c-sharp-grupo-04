c64f77fe47d0242cb4e51f38cfe360da5a2049a6
PereiraMarcos5 <38700005+PereiraMarcos5@users.noreply.github.com>
2018-08-30 16:23:08 -0300

    Feito Cadastro para Gastos com o Ajax

---
+++ SistemaFinanceiro/Scripts/scriptGastos/create-gasto.js
@@ -0,0 +1,33 @@
+﻿$(function () {
+
+    $('body').on('click', '#salvar-cadastro-gasto', function () {
+        $.ajax({
+            url: '/Gasto/CadastroModalGastos',
+            method: 'POST',
+            data: {
+                ValorGastos: $('#campo-valor').val(),
+                DataEntrada: $('#campo-data-entrada').val(),
+                DataVencimento: $('#campo-vencimento').val(),
+                Descricao: $('#campo-descricao').val(),
+            },
+            success: function (data) {
+                limpaCampos();
+                $('#cadastrar-gastos-modal').modal('hide');
+                $('#tabela-gastos').DataTable().ajax.reload();
+            }
+        });
+    });
+    $('#cancelar-cadastro-gastos').on('click', function () {
+        limpaCampos();
+    });
+
+    function limpaCampos() {
+
+        $('#campo-valor').val('');
+        $('#campo-data-entrada').val('');
+        $('#campo-vencimento').val('');
+        $('#campo-descricao').val('');
+    };
+
+
+});
