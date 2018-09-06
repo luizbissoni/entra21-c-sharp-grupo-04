3017a8a3a97bb1d990b792b487e72ceef8e0fb20
PereiraMarcos5 <38700005+PereiraMarcos5@users.noreply.github.com>
2018-09-06 14:14:37 -0300

    estrutura

--- Repository/DataBase/Estrutura.sql
+++ Repository/DataBase/Estrutura.sql
@@ -65,11 +65,12 @@ INSERT INTO pessoas (nome, sexo, cpf, nascimento) VALUES
 ('João da Silva', 'M', '123.456.789-98', '1998-06-05'),
 ('Rute da Silva', 'F', '987.654.321-98', '1945-12-01');
 
---INSERT INTO cartoes (id_pessoas, numero, conta, bandeira, banco) values((SELECT Id FROM pessoas WHERE pessoas.nome = 'João da Silva'), '123456', '123456', 'VISA', 'BRADESCO');
+INSERT INTO cartoes (id_pessoas, numero, conta, bandeira, banco) values((SELECT Id FROM pessoas WHERE pessoas.nome = 'João da Silva'), '123456', '123456', 'VISA', 'BRADESCO');
 
 
-INSERT INTO [login] (usuario, senha, email) VALUES ('admin', '123', 'admin@admin.com'),('nene','123','neneNanight@lele.com.br');
+INSERT INTO [login] (id_pessoas, usuario, senha, email) VALUES (1, 'admin', '123', 'admin@admin.com'),(null, 'nene','123','neneNanight@lele.com.br');
 
 SELECT * FROM pessoas;
 INSERT INTO categorias (nome) VALUES('aluguel'),('vestuario'),('alimento')
 --SELECT pessoas.Id, pessoas.nome, login.id_pessoas, login.usuario, login.email FROM login INNER JOIN pessoas ON login.id_pessoas = pessoas.Id
+--update login set login.id_pessoas = 1 where login.usuario = 'admin';
