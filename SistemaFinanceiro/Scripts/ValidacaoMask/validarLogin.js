44928561ac933b26de63570f0e3e700f535289f8
Luiz Antonio Bissoni <labissoni@gmail.com>
2018-09-18 15:58:03 -0300

    .

---
+++ SistemaFinanceiro/Scripts/ValidacaoMask/validarLogin.js
@@ -0,0 +1,36 @@
+﻿$(document).ready(function () {
+    $('#validarLogin').validate({
+        errorClass: "form-control-danger",
+        validClass: "form-control-success",
+        highlight: function (element) {
+            jQuery(element).closest('.form-group').addClass('has-error');
+        },
+        unhighlight: function (element) {
+            jQuery(element).closest('.form-group').removeClass('has-error');
+        },
+        errorPlacement: function (error, element) {
+            $(element).parent().append(error[0])
+        },
+        rules: {
+            "campo-usuario": {
+                required: true,
+            },
+            "campo-senha": {
+                required: true,
+                rangelength: [6,10]
+            }
+        },
+        menssage: {
+            "campo-usuario": {
+                required: "Este campo é requerido.",
+            },
+            "campo-senha": {
+                required: "Este campo é requerido.",
+                rangelength: "Por favor, forneça um valor entre {0} e {1} caracteres de comprimento."
+
+            }
+        }
+
+
+    });
+});
