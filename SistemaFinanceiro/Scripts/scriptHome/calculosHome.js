fb2946caacc9bfc9fb61c6f97f44c066286704fe
Luiz Antonio Bissoni <labissoni@gmail.com>
2018-09-17 21:33:06 -0300

    grafico index

--- SistemaFinanceiro/Scripts/scriptHome/calculosHome.js
+++ SistemaFinanceiro/Scripts/scriptHome/calculosHome.js
@@ -1,24 +1,27 @@
 ﻿$(document).ready(function () {
 
-    var valorRecebido, valorGasto, porcentagemGasto;
-   
-   // $(".porcento").attr("data-percent", porcentagemGasto);
+    var valorRecebido, valorGasto, porcentagemGasto, porcentoTotalCarteira;
+
 
     $.ajax({
         url: '/Home/TotalRecebido',
         method: 'GET',
         success: function (resultado) {
             var total = JSON.parse(resultado);
-            //console.log(total);
+            valorRecebido = total.tabela[0].total;
+
+            porcentoTotalCarteira = (valorGasto * 100) / valorRecebido;
+
+            $("#porcentoCarteira").append('<div class="chart" data-percent="' + porcentoTotalCarteira + '" data-barcolor="#4680FE" data-trackcolor="#dbdada" data-linewidth="6" data-barsize="110"><div class="chart-percent"><span></span >%</div ></div > ');
             if (total.tabela[0].total > 0) {
-                $('h2.saldo-recebido').number('R$' + total.tabela[0].total, 2, ',', '.')
-                //$('.saldo-recebido').text('R$' + total.tabela[0].total);
-                valorRecebido = total.tabela[0].total;
+                $('.saldo-recebido').text('R$' + total.tabela[0].total);
             } else {
-                $('.saldo-recebido').text('R$' + 00,00);
-                valorRecebido = total.tabela[0].total;
+               // $("#porcentoCarteira").append('<div class="chart" data-percent="0" data-barcolor="#4680FE" data-trackcolor="#dbdada" data-linewidth="6" data-barsize="110"><div class="chart-percent"><span></span>%</div></div>');
+                //$('.saldo-recebido').text('R$' + 00,00);
             }
-           
+            //$('h2.saldo-recebido').number('R$' + total.tabela[0].total, 2, ',', '.')
+            //valorRecebido = total.tabela[0].total;
+
         }
     });
 
@@ -28,14 +31,12 @@
         success: function (resultado) {
             var total = JSON.parse(resultado);
             var totalGasto = total.tabela[0].total;
-            $('#total-gastos').text('R$' + totalGasto).maskMoney();
-            //console.log(total.tabela[0].total);
+            $('#total-gastos').text('R$' + totalGasto);
             valorGasto = total.tabela[0].total
-          
+
             porcentagemGasto = ((valorRecebido - valorGasto) / valorRecebido) * 100;
-            //$('#meu-teste').data('percente')
-            //$('#meu-teste').attr('data-percent', porcentagemGasto);
-            //console.log(porcentagemGasto);
+
+            $("#teste").append('<div class="chart dial" data-percent="' + porcentagemGasto + '" data-barcolor="#FC6180" data-trackcolor="#dbdada" data-linewidth="6" data-barsize="110"><div class="chart-percent"><span></span>%</div></div>');
         }
     });
 
@@ -44,7 +45,6 @@
         method: 'GET',
         success: function (resultado) {
             var total = JSON.parse(resultado);
-            //console.log(total);
             // $('.setor-maior-gasto').text('R$' + total.tabela[0].total);
         }
     });
@@ -59,7 +59,9 @@
 
         }
     });
-    
+
+
+
 
 });
 
