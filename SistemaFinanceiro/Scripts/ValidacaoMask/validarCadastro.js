4164344be0edbd835e3f2c695828ac150253e9eb
Leonardo Forbici <leonardoforbici1@gmail.com>
2018-09-18 22:28:34 -0300

    Criando validacao de data.

--- SistemaFinanceiro/Scripts/ValidacaoMask/validarCadastro.js
+++ SistemaFinanceiro/Scripts/ValidacaoMask/validarCadastro.js
@@ -1,9 +1,28 @@
-﻿$ (function () {
+﻿$.('document').ready(); {
+
+$.validator.addMethod("#cadastro-nascimento", function (value, element) {
+    if (value.length != 10) return false;
+    // verificando data
+    var data = value;
+    var dia = data.substr(0, 2);
+    var barra1 = data.substr(2, 1);
+    var mes = data.substr(3, 2);
+    var barra2 = data.substr(5, 1);
+    var ano = data.substr(6, 4);
+    if (data.length != 10 || barra1 != "/" || barra2 != "/" || isNaN(dia) || isNaN(mes) || isNaN(ano) || dia > 31 || mes > 12) return false;
+    if ((mes == 4 || mes == 6 || mes == 9 || mes == 11) && dia == 31) return false;
+    if (mes == 2 && (dia > 29 || (dia == 29 && ano % 4 != 0))) return false;
+    if (ano < 1900) return false;
+    return true;
+}
+
+
+$(function () {
 
     $('#form-cadastro-pessoa').validate({
-        
+
         errorClass: "form-control-danger",
-        highlight: function (element){
+        highlight: function (element) {
             JQuery(element).closest('.form-group').addClass('has-error');
         },
         unhighlight: function (element) {
@@ -14,22 +33,23 @@
         },
 
         rules: {
-            'cadastro-nome':{
+            'cadastro-nome': {
                 number: false,
                 required: true,
                 rengelength: [8, 100]
             },
-            'cadastro-nascimento':{
+            'cadastro-nascimento': {
                 required: true,
-                  
+
+
             },
-            'cadastro-telefone':{
+            'cadastro-telefone': {
                 required: true,
-               
+
             },
             'cadastro-cpf': {
-                required: true,
-            }            
+                    required: true
+}            
         },
         messages:{
 
