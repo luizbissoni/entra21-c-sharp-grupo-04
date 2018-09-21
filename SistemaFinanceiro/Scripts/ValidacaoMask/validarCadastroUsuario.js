782743656396be64739fb0f17386ef6f3b9ca23b
Leonardo Forbici <38699633+LeonardoForbici@users.noreply.github.com>
2018-09-21 17:15:57 -0300

    Verificar CPF .

--- SistemaFinanceiro/Scripts/ValidacaoMask/validarCadastroUsuario.js
+++ SistemaFinanceiro/Scripts/ValidacaoMask/validarCadastroUsuario.js
@@ -19,7 +19,7 @@
         rules: {
             'nome': {
                 required: true,
-               // rangelenght: [8,100]
+                // rangelenght: [8,100]
             },
             'nascimento': {
                 required: true,
@@ -27,11 +27,11 @@
             },
             'telefone': {
                 required: true,
-               // rangelenght:[10,11]
+                // rangelenght:[10,11]
             },
             'new-CPF': {
-                required: true,
-              //  rangelength: [  ]
+                required: true, verificarCPF: true,
+                //  rangelength: [  ]
             },
             'Cep': {
                 required: true,
@@ -64,7 +64,7 @@
             'telefone': {
                 required: "Este campo é requerido.",
             },
-            'new-CPF ': {
+            'new-CPF': {
                 required: "Este campo é requerido.",
             },
             'Cep': {
@@ -83,7 +83,7 @@
             },
             'repeatPasswordInput': {
                 required: "Este campo é requerido.",
-               // equalTo: 'Senhas não conferem'
+                // equalTo: 'Senhas não conferem'
             }
         }
        
@@ -95,53 +95,164 @@
     $('.close-cadastro').on('click', function () {
         $('[data-toggle="tooltip"], .tooltip').tooltip("hide");
     });
-});
+    jQuery.validator.addMethod("verificaCPF", function (value, element) {
 
+        value = value.replace('.', '');
+        value = value.replace('.', '');
+        cpf = value.replace('-', '');
 
-/*function valCpf($cpf) {
-    $cpf = preg_replace('/[^0-9]/', '', $cpf);
-    $digitoA = 0;
-    $digitoB = 0;
-    for ($i = 0, $x = 10; $i <= 8; $i++ , $x--) {
-        $digitoA += $cpf[$i] * $x;
-    }
-    for ($i = 0, $x = 11; $i <= 9; $i++ , $x--) {
-        if (str_repeat($i, 11) == $cpf) {
-            return false;
+        while (cpf.length < 11) cpf = "0" + cpf;
+        var expReg = /^0+$|^1+$|^2+$|^3+$|^4+$|^5+$|^6+$|^7+$|^8+$|^9+$/;
+        var a = [];
+        var b = new Number;
+        var c = 11;
+
+        for (i = 0; i < 11; i++) {
+            a[i] = cpf.charAt(i);
+            if (i < 9) b += (a[i] * --c);
         }
-        $digitoB += $cpf[$i] * $x;
-    }
-    $somaA = (($digitoA % 11) < 2) ? 0 : 11 - ($digitoA % 11);
-    $somaB = (($digitoB % 11) < 2) ? 0 : 11 - ($digitoB % 11);
-    if ($somaA != $cpf[9] || $somaB != $cpf[10]) {
-        return false;
-    } else {
-        return true;
-    }
-}
 
-function validaDat(campo, valor) {
-    var date = valor;
-    var ardt = new Array;
-    var ExpReg = new RegExp("(0[1-9]|[12][0-9]|3[01])/(0[1-9]|1[012])/[12][0-9]{3}");
-    ardt = date.split("/");
-    erro = false;
-    if (date.search(ExpReg) == -1) {
-        erro = true;
+        if ((x = b % 11) < 2) { a[9] = 0 } else { a[9] = 11 - x }
+
+        b = 0;
+        c = 11;
+
+        for (y = 0; y < 10; y++) b += (a[y] * c--);
+
+        if ((x = b % 11) < 2) { a[10] = 0; } else { a[10] = 11 - x; }
+
+        if ((cpf.charAt(9) != a[9]) || (cpf.charAt(10) != a[10]) || cpf.match(expReg)) return false;
+
+        var verifica = false;
+        $.get('verificaCpf.php', { cpf: cpf }, function (data) {
+            if (data == 0) verifica = true;
+        });
+
+        if (!verifica) return false;
+
+        return true;
+    }, "CPF inválido ou já cadastrado!");
+  });
+    /*  $.validator.addMethod("new-CPF", function (value) {
+          if (value.length != 10) return false;
+          $cpf = preg_replace('/[^0-9]/', '', $cpf);
+          $digitoA = 0;
+          $digitoB = 0;
+          for ($i = 0, $x = 10; $i <= 8; $i++, $x--) {
+              $digitoA += $cpf[$i] * $x;
+          }
+          for ($i = 0, $x = 11; $i <= 9; $i++, $x--) {
+              if (str_repeat($i, 11) == $cpf) {
+                  return false;
+              }
+              $digitoB += $cpf[$i] * $x;
+          }
+          $somaA = (($digitoA % 11) < 2) ? 0 : 11 - ($digitoA % 11);
+          $somaB = (($digitoB % 11) < 2) ? 0 : 11 - ($digitoB % 11);
+          if ($somaA != $cpf[9] || $somaB != $cpf[10]) {
+              return false;
+          } else {
+              return true;
+          }
+      }, "Por favor, forneça uma data válida.");
+  
+  
+  
+  
+  function valCpf($cpf) {
+      $cpf = preg_replace('/[^0-9]/', '', $cpf);
+      $digitoA = 0;
+      $digitoB = 0;
+      for ($i = 0, $x = 10; $i <= 8; $i++ , $x--) {
+          $digitoA += $cpf[$i] * $x;
+      }
+      for ($i = 0, $x = 11; $i <= 9; $i++ , $x--) {
+          if (str_repeat($i, 11) == $cpf) {
+              return false;
+          }
+          $digitoB += $cpf[$i] * $x;
+      }
+      $somaA = (($digitoA % 11) < 2) ? 0 : 11 - ($digitoA % 11);
+      $somaB = (($digitoB % 11) < 2) ? 0 : 11 - ($digitoB % 11);
+      if ($somaA != $cpf[9] || $somaB != $cpf[10]) {
+          return false;
+      } else {
+          return true;
+      }
+  }
+  
+   function validaDat(campo, valor) {
+      var date = valor;
+      var ardt = new Array;
+      var ExpReg = new RegExp("(0[1-9]|[12][0-9]|3[01])/(0[1-9]|1[012])/[12][0-9]{3}");
+      ardt = date.split("/");
+      erro = false;
+      if (date.search(ExpReg) == -1) {
+          erro = true;
+      }
+      else if (((ardt[1] == 4) || (ardt[1] == 6) || (ardt[1] == 9) || (ardt[1] == 11)) && (ardt[0] > 30))
+          erro = true;
+      else if (ardt[1] == 2) {
+          if ((ardt[0] > 28) && ((ardt[2] % 4) != 0))
+              erro = true;
+          if ((ardt[0] > 29) && ((ardt[2] % 4) == 0))
+              erro = true;
+      }
+      if (erro) {
+          alert(" valor "+" não é uma data válida!!!");
+          campo.focus();
+          campo.value = "";
+          return false;
+      }
+      return true; 
+  } */
+
+/*jQuery.validator.addMethod("verificaCPF", function (value, element) {
+
+    value = value.replace('.', '');
+    value = value.replace('.', '');
+    cpf = value.replace('-', '');
+
+    while (cpf.length < 11) cpf = "0" + cpf;
+    var expReg = /^0+$|^1+$|^2+$|^3+$|^4+$|^5+$|^6+$|^7+$|^8+$|^9+$/;
+    var a = [];
+    var b = new Number;
+    var c = 11;
+
+    for (i = 0; i < 11; i++) {
+        a[i] = cpf.charAt(i);
+        if (i < 9) b += (a[i] * --c);
     }
-    else if (((ardt[1] == 4) || (ardt[1] == 6) || (ardt[1] == 9) || (ardt[1] == 11)) && (ardt[0] > 30))
-        erro = true;
-    else if (ardt[1] == 2) {
-        if ((ardt[0] > 28) && ((ardt[2] % 4) != 0))
-            erro = true;
-        if ((ardt[0] > 29) && ((ardt[2] % 4) == 0))
-            erro = true;
-    }
-    if (erro) {
-        alert(" valor "+" não é uma data válida!!!");
-        campo.focus();
-        campo.value = "";
-        return false;
-    }
-    return true; 
-} */
+
+    if ((x = b % 11) < 2) { a[9] = 0 } else { a[9] = 11 - x }
+
+    b = 0;
+    c = 11;
+
+    for (y = 0; y < 10; y++) b += (a[y] * c--);
+
+    if ((x = b % 11) < 2) { a[10] = 0; } else { a[10] = 11 - x; }
+
+    if ((cpf.charAt(9) != a[9]) || (cpf.charAt(10) != a[10]) || cpf.match(expReg)) return false;
+
+    var verifica = false;
+    $.get('verificaCpf.php', { cpf: cpf }, function (data) {
+        if (data == 0) verifica = true;
+    });
+
+    if (!verifica) return false;
+
+    return true;
+}, "CPF inválido ou já cadastrado!");
+*/
+
+/*$("#form").validate({
+    rules:
+    {
+        nome:{required: true},
+        senha:{required: true, minlength: 5},
+        cpf:{required: true, verificaCPF: true}
+       }
+}); */
+// Não esquecer de colocar nas regras ( para chamar a validação do CPF )
+//http://flaviosilveira.com/2010/jquery-jquery-validation-sincrono-e-assincrono-cpf-unico-no-banco-de-dados-ajax-sincrono-com-jquery/
