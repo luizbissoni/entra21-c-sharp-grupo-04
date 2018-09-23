6ffe03077c0251abdea56cde21ae47ed4278a626
Luiz Antonio Bissoni <labissoni@gmail.com>
2018-09-23 14:05:37 -0300

    revisando CRUD calendario

--- SistemaFinanceiro/Scripts/scriptHome/calendario.js
+++ SistemaFinanceiro/Scripts/scriptHome/calendario.js
@@ -1,20 +1,38 @@
 ﻿$(document).ready(function () {
+    $('#calendario-numero-cartao-editar').select2({
+        placeholder: "selecione o cartão",
+        ajax: {
+            url: '/Cartao/ObterTodosParaJson',
+            dataType: 'json',
+        }
+    }); //preenche select2 cartao calendario
+
+    $('#campo-calendario-descricao-editar').select2({
+        placeholder: "selecione a categoria",
+        ajax: {
+            url: '/Categoria/ObterTodosCategoriaJson',
+            dataType: 'json'
+        },
+
+    }); //preenche select2 categoria calendario
 
     var events = [];
     var selectedEvent = null;
 
+    //pusher inicio
     Pusher.logToConsole = false;
-
     var pusher = new Pusher('3d2e47e4a257a668b2cc', {
         cluster: 'us2',
         forceTLS: true
     });
-
     var channel = pusher.subscribe('my-channel');
     channel.bind('cadastroGastos', function (data) {
         var resultado = JSON.stringify(data);
         $('#calendario').fullCalendar('refetchEvents');
     });
+    //pusher fim
+
+
 
     preencherFullCalendar();
 
@@ -44,72 +62,75 @@
         });
     }
 
-
     function generationCalendar(events) {
         $('#calendario').fullCalendar('destroy');
-    $('#calendario').fullCalendar({
-        locale: "pt-BR",
-        height: 550,
-        header: {
-            left: 'prev,next today',
-            center: 'title',
-            right: 'month,agendaWeek,agendaDay,listMonth'
-        },
+        $('#calendario').fullCalendar({
+            locale: "pt-BR",
+            height: 550,
+            header: {
+                left: 'prev,next today',
+                center: 'title',
+                right: 'month,agendaWeek,agendaDay,listMonth'
+            },
 
-        eventStartEditable: true,
-        eventLimit: true,
-        eventDurationEditable: true,
-        navLinks: true, // can click day/week names to navigate views
-        businessHours: true, // display business hours
-        editable: true,
-        selectHelper: true,
-        selectable: true,
-        select: function (start, end) {
+            eventStartEditable: true,
+            eventLimit: true,
+            eventDurationEditable: true,
+            navLinks: true, // can click day/week names to navigate views
+            businessHours: true, // display business hours
+            editable: true,
+            selectHelper: true,
+            selectable: true,
+            select: function (start, end) {
 
-            $('#start').val(moment(start).format("L LT"));
-            $('#end').val(moment(end).format("L LT"));
+                $('#start').val(moment(start).format("L LT"));
+                $('#end').val(moment(end).format("L LT"));
 
-            $("#modal-cadastro-gasto-calendario").modal('show');
+                $("#modal-cadastro-gasto-calendario").modal('show');
 
-        },
-        droppable: true, // this allows things to be dropped onto the calendar
-        drop: function (date) {
-            alert(date);
-        },
-        events: events,
-        eventRender: function (event, element) {
-            if (element && event.title) {
-                element.qtip({
-                    content: event.title,
-                    hide: {
-                        fixed: true,
-                        delay: 500
-                    }
-                });
-            }
-        },
-        eventClick: function (event) {
-            selectedEvent = event;
-            $('#modal-visualizar-evento #title').text(event.title);
-            $('#modal-visualizar-evento #start').text(moment(event.start).format("L LT"));
-            $('#modal-visualizar-evento #end').text(moment(event.end).format("L LT"));
-            $('#modal-visualizar-evento').modal('show');
+            },
+            droppable: true, // this allows things to be dropped onto the calendar
+            drop: function (date) {
+                alert(date);
+            },
+            events: events,
+            eventRender: function (event, element) {
+                if (element && event.title) {
+                    element.qtip({
+                        content: event.title,
+                        hide: {
+                            fixed: true,
+                            delay: 500
+                        }
+                    });
+                }
+            },
+            eventClick: function (event) {
+                selectedEvent = event;
+                $('#modal-visualizar-evento #title').text(event.title);
+                $('#modal-visualizar-evento #start').text(moment(event.start).format("L LT"));
+                $('#modal-visualizar-evento #end').text(moment(event.end).format("L LT"));
+                $('#modal-visualizar-evento').modal('show');
 
-            return false;
-        },
-        eventAfterRender: function (event, element, view) {
-            var dataComparar = moment(event.start).format("L");
-            //birthday = new Date('<somedate>');
-            year = new Date(event.start).getFullYear();
-            month = new Date(event.start).getMonth();
-            day = new Date(event.start).getDate();
-            moment(event.end).format("L");
-        },
+                return false;
+            },
+            eventAfterRender: function (event, element, view) {
+                var dataComparar = moment(event.start).format("L");
+                //birthday = new Date('<somedate>');
+                year = new Date(event.start).getFullYear();
+                month = new Date(event.start).getMonth();
+                day = new Date(event.start).getDate();
+                moment(event.end).format("L");
+            },
 
         });
     }
 
-    var table = $('#tabela-teste').DataTable(); 
+
+
+
+
+    var table = $('#tabela-teste').DataTable();
     //Exclui evento do calendario
     $('#excluirEvento').on('click', function () {
         if (selectedEvent != null && confirm('Tem certeza que deseja excluir esse evento ?')) {
@@ -142,8 +163,43 @@
         }
     });
 
-  
+    //BOTAO SALVAR EDIÇÃO GASTOS NO CALENDARIO
+    $('#botao-modal-editar-gasto-calendario').on('click', function () {
+        $valor = $('#modal-editar-gasto-calendario #campo-calendario-valor-editar').val(),
+            $valor = $valor.replace(/\,/g, ""),
+            $valor = $valor.replace('.', ","),
+            $.ajax({
+                url: '/Home/UpdateGastos',
+                method: 'POST',
+                data: {
+                    Id: selectedEvent.id,
+                    idCartao: $('#modal-editar-gasto-calendario #calendario-numero-cartao-edita').val(),
+                    idCategoria:  $('#modal-editar-gasto-calendario #campo-calendario-descricao-editar').val(),
+                    Valor: $valor,
+                    entrada: $('#modal-editar-gasto-calendario #calendario-descricao-despesa-editar').val(),
+                    vencimento: $('#modal-editar-gasto-calendario #start-editar').val(),
+                    descricao:  $('#modal - editar - gasto - calendario #end - editar').val()
+                },
+                success: function () {
+                    table.ajax.reload();
+                    preencherFullCalendar();
+                    $('#modal-editar-gasto-calendario').modal('hide');
+                    new PNotify({
+                        text: 'Gasto editado com sucesso.',
+                        type: 'success'
+                    });
+                },
+                error: function () {
+                    new PNotify({
+                        text: 'Algo deu errado.',
+                        icon: 'icofont icofont-info-circle',
+                        type: 'error'
+                    });
+                }
+            });
+    });
 
+    //CADASTRAR GASTOS CALENDARIO
     $('#salvar-gastos-calendario').on('click', function () {
 
         $valor = $('#campo-calendario-valor').val();
@@ -182,31 +238,40 @@
         });
     });
 
+    //MODAL EDITAR GASTOS CALENDARIO
     $('#editarEvento').on('click', function () {
         $('#modal-visualizar-evento').modal('hide');
-        $('#modal-cadastro-gasto-calendario').modal('show');
+        $.ajax({
+            url: '/Home/EditarGastos',
+            method: 'GET',
+            data: {
+                Id: selectedEvent.id
+            },
+            async: false,
+            success: function (pesquisa) {
+                var resultado = JSON.parse(pesquisa);
+                $('#modal-editar-gasto-calendario #calendario-numero-cartao-edita').append(new Option(resultado.gastos.cartao.Conta, resultado.gastos.IdCartao, false, false)).val(resultado.gastos.IdCartao).trigger('change');
+                $('#modal-editar-gasto-calendario #campo-calendario-descricao-editar').append(new Option(resultado.gastos.Categoria.Nome, resultado.gastos.IdCategoria, false, false)).val(resultado.gastos.IdCategoria).trigger('change');
+                $('#modal-editar-gasto-calendario #campo-calendario-valor-editar').val(resultado.gastos.Valor);
+                $('#modal-editar-gasto-calendario #calendario-descricao-despesa-editar').val(resultado.gastos.Descricao);
+                $('#modal-editar-gasto-calendario #start-editar').val(resultado.gastos.Entrada);
+                $('#modal-editar-gasto-calendario #end-editar').val(resultado.gastos.Vencimento);
+            },
+            error: function () {
+                new PNotify({
+                    text: 'Algo deu errado.',
+                    icon: 'icofont icofont-info-circle',
+                    type: 'error'
+                });
+            }
+        });
     });
 
     $('.fechar-cadastro-gasto-calendario').on('click', function () {
         limparCampos();
     });
 
-    $('#campo-calendario-numero-cartao').select2({
-        placeholder: "selecione o cartão",
-        ajax: {
-            url: '/Cartao/ObterTodosParaJson',
-            dataType: 'json',
-        }
-    }); //preenche select2 cartao calendario
 
-    $('#campo-calendario-descricao').select2({
-        placeholder: "selecione a categoria",
-        ajax: {
-            url: '/Categoria/ObterTodosCategoriaJson',
-            dataType: 'json'
-        },
-
-    }); //preenche select2 categoria calendario
 
     function limparCampos() {
 
@@ -216,4 +281,5 @@
         $('#campo-calendario-valor').val('');
         $('#calendario-descricao-despesa').val('');
     }
+
 });
